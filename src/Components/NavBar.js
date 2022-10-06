24aaa78eac098ffab5609c0d175f9cbc61b76c00
deboeliza <deboeliza2015@gmail.com>
2022-10-06 12:21:24 -0300

    reentrega

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import CardWidget from './CardWidget'
+
+function NavBar () {
+     return (
+        <div>
+         <h2>FLORENCIA INDUMENTARIA</h2>
+         <ul>
+            <li>Home</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+         </ul>
+         <CardWidget/>
+        </div>
+     )
+}
+
+export default NavBar;
